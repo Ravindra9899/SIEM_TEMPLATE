475a04c87d6c80e8582bce3c1ff58df0beb0b3e5
Naman Mishra <namanmishraec1045@gmail.com>
2023-03-28 13:20:44 +0530

    Version: Mar 28 1317: Updates: single view report templated

--- src/app/services/reader.service.ts
+++ src/app/services/reader.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReaderService {
 
-  constructor() { }
+  private url = '/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  apiCallToGetSingleViewAsn(ipAddress: string): Observable<any> {
+
+    const requestUri1 = this.url + `/single-view-asn?ipAddress=${ipAddress}`;
+
+    return this.httpClient.get(requestUri1);
+  }
 }
--- src/app/views/scanned-ip-list/single-view/single-view.component.ts
+++ src/app/views/scanned-ip-list/single-view/single-view.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ReaderService } from 'src/app/services/reader.service';
 
 @Component({
   selector: 'app-single-view',
@@ -12,19 +13,31 @@ export class SingleViewComponent implements OnInit, AfterViewInit {
 
   constructor(
     private route: ActivatedRoute,
-    private elementRef: ElementRef
+    private elementRef: ElementRef,
+    private readerService: ReaderService
   ) { }
 
   ngOnInit(): void {
     this.ipAddress = this.route?.snapshot?.queryParamMap.get('ipAddress') || '';
+
+    this.readerService.apiCallToGetSingleViewAsn(this.ipAddress).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.error('Error in sending single view asn response ', err);
+      },
+      complete: () => {
+        console.info('Single view ASN request completed');
+      }
+    });
   }
 
   ngAfterViewInit(): void {
-    setTimeout(() => {
-      const scoreSpan = this.elementRef.nativeElement.querySelector('.threat-score-div span');
-      scoreSpan.insertAdjacentHTML('afterend', '<div>New element appended after score</div>');
+    // setTimeout(() => {
+    //   const scoreSpan = this.elementRef.nativeElement.querySelector('.threat-score-div span');
+    //   scoreSpan.insertAdjacentHTML('afterend', '<div>New element appended after score</div>');
 
-    }, 1500);
+    // }, 1500);
   }
-
 }
